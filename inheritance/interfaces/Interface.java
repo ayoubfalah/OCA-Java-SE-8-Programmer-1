caf08ef8c7f973f9e1e059e7e072de703cbcfcc6
Ayoub Falah <ayoub_falah@hotmail.de>
2016-12-25 12:52:28 +0100

    The elements of an interface

---
+++ inheritance/interfaces/Interface.java
@@ -0,0 +1,13 @@
+package inheritance.interfaces;
+
+/**
+ *
+ * @author ayoubfalah
+ */
+public interface Interface 
+{
+    static final int CONSTANT = 2;
+    public void abstractMethod();
+    public default String methodWithDefaultImplementation(){ return "Hello!"; }
+    public static String staticMethod(){ return "Static method!"; }
+}
