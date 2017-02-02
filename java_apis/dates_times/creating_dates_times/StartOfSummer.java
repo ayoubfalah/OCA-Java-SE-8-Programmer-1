975483e1a20e2d1fc899558e9cd64310ac2a1612
Ayoub Falah <ayoub_falah@hotmail.de>
2017-02-02 09:31:54 +0100

    Creating LocalDate objects

---
+++ java_apis/dates_times/creating_dates_times/StartOfSummer.java
@@ -0,0 +1,11 @@
+package java_apis.dates_times.creating_dates_times;
+
+import java.time.*;
+
+public class StartOfSummer
+{
+   public static void main(String... args)
+   {
+      LocalDate date = LocalDate.of(2014, Month.JUNE, 21);
+   }
+}
