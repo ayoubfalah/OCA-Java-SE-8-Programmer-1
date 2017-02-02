e6e7474b615f51f5ae8ccb18b4446cdf364c6e41
Ayoub Falah <ayoub_falah@hotmail.de>
2017-02-02 08:31:20 +0100

    static initializer block

---
+++ methods_encapsulation/static_initialization/Deer.java
@@ -0,0 +1,18 @@
+package methods_encapsulation.static_initialization;
+
+public class Deer
+{
+   static int count;
+   static { count = 0;}
+
+   Deer(){ count++; }
+   
+   public static void main(String... args)
+   {
+     count++;
+     Deer mother = new Deer();
+     Deer father = new Deer();
+     Deer fawn = new Deer();
+     System.out.println(father.count);
+   }
+}
